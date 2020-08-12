26ae6d88552725ee76a04e3e762fc3fefe751457
Irwan Setiawan <i.setiawan@criteo.com>
2020-08-12 10:23:22 +0800

    Fix license header

--- AppBidding-MoPub/AppBidding-MoPub/AppDelegate.h
+++ AppBidding-MoPub/AppBidding-MoPub/AppDelegate.h
@@ -1,4 +1,8 @@
-//  Copyright © 2018-2020 Criteo. All rights reserved.
+//
+//  AppDelegate.h
+//  AppBidding-MoPub
+//
+//  Copyright © 2020 Criteo. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,10 +15,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//  AppDelegate.h
-//  AppBidding-MoPub
-//
-//  Copyright © 2020 Criteo. All rights reserved.
 //
 
 
--- AppBidding-MoPub/AppBidding-MoPub/InterstitialViewController.m
+++ AppBidding-MoPub/AppBidding-MoPub/InterstitialViewController.m
@@ -1,4 +1,8 @@
-//  Copyright © 2018-2020 Criteo. All rights reserved.
+//
+//  InterstitialViewController.m
+//  AppBidding-MoPub
+//
+//  Copyright © 2020 Criteo. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,10 +15,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//  InterstitialViewController.m
-//  AppBidding-MoPub
-//
-//  Copyright © 2020 Criteo. All rights reserved.
 //
 
 
